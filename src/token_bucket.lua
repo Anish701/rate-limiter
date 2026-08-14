ba8917bf848fb4f0971276d6863f5debf4e193b8
Anish701 <anish2sinha@gmail.com>
2026-08-14 11:53:04 -0400

    fix: lua script
    Signed-off-by: Anish701 <anish2sinha@gmail.com>

--- src/token_bucket.lua
+++ src/token_bucket.lua
@@ -1,35 +1,37 @@
 local key = KEYS[1]
 
-local max_tokens = ARGS[1]
-local refill_rate = ARGS[2]
-local curr_time = ARGS[3]
-local requested_tokens = ARGS[4]
+local max_tokens = tonumber(ARGV[1])
+local refill_rate = tonumber(ARGV[2])
+local curr_time = tonumber(ARGV[3])
+local requested_tokens = tonumber(ARGV[4])
 
 -- current bucket state
-
 local data = redis.call('HMGET', key, 'tokens', 'last_refill')
-local tokens = data[1]
-local last_refill = data[2]
+local tokens = tonumber(data[1])
+local last_refill = tonumber(data[2])
 
 if not tokens or not last_refill then
     tokens = max_tokens
-    last_refill = now
+    last_refill = curr_time
 else
     -- refill calculation
-    missing_tokens = math.max(0, now - last_refill) * refill_rate
+    local missing_tokens = math.max(0, curr_time - last_refill) * refill_rate
     tokens = math.min(max_tokens, tokens + missing_tokens)
-    last_refill = now
+    last_refill = curr_time
 end
 
--- deduct tokens for a request
+local allowed = 0
+
+-- approve request if enough tokens
 if tokens >= requested_tokens then
     tokens = tokens - requested_tokens
-    redis.call('HMSET', key, 'tokens', tokens, 'last_refill', last_refill)
-    local.ttl = math.ceil(max_tokens/refill_rate * 3)
-    redis.call('EXPIRE', key, ttl)
-    return 1
-else
-    redis.call('HMSET', key, 'tokens', tokens, 'last_refill', last_refill)
-    local.ttl = math.ceil(max_tokens/refill_rate * 3)
-    redis.call('EXPIRE', key, ttl)
-    return 0
+    allowed = 1
+end
+
+-- update bucket state
+redis.call('HMSET', key, 'tokens', tokens, 'last_refill', last_refill)
+
+local ttl = math.ceil((max_tokens / refill_rate) * 3)
+redis.call('EXPIRE', key, ttl)
+
+return allowed
